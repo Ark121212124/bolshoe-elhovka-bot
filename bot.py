9ba0a8aa38fe12a3bab5a6a4ea574973b1be48a3
Ark121212124 <Arktiko12@gmail.com>
2026-01-23 23:43:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,15 +8,15 @@ from telegram.ext import (
 from config import TOKEN
 from handlers.start import start
 from handlers.menu import text_menu_handler
+from handlers.news import news_photo_handler
 
 
 def main():
     app = Application.builder().token(TOKEN).build()
 
-    # /start
     app.add_handler(CommandHandler("start", start))
 
-    # 🔘 ВСЕ КНОПКИ ReplyKeyboard
+    app.add_handler(MessageHandler(filters.PHOTO, news_photo_handler))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, text_menu_handler))
 
     print("🚀 Бот запущен")
