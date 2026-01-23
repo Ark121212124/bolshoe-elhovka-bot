d49f21e094fdf3ba66508f91671e60907635df2a
Ark121212124 <Arktiko12@gmail.com>
2026-01-23 22:45:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,17 +2,12 @@ from telegram.ext import (
     Application,
     CommandHandler,
     MessageHandler,
-    CallbackQueryHandler,
     filters,
 )
-from config import TOKEN
 
+from config import TOKEN
 from handlers.start import start
 from handlers.menu import text_menu_handler
-from handlers.news import news_callbacks
-from handlers.contacts import contacts_callbacks
-from handlers.appeals import appeals_callbacks
-from handlers.subscriptions import subscriptions_callbacks
 
 
 def main():
@@ -21,14 +16,8 @@ def main():
     # /start
     app.add_handler(CommandHandler("start", start))
 
-    # ⬇️ ОБРАБОТКА ТЕКСТОВЫХ КНОПОК (ReplyKeyboard)
+    # 🔘 ВСЕ КНОПКИ ReplyKeyboard
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, text_menu_handler))
-
-    # ⬇️ inline-кнопки (ОСТАВЛЯЕМ)
-    app.add_handler(CallbackQueryHandler(news_callbacks, pattern="^news_"))
-    app.add_handler(CallbackQueryHandler(contacts_callbacks, pattern="^contacts_"))
-    app.add_handler(CallbackQueryHandler(appeals_callbacks, pattern="^appeal_"))
-    app.add_handler(CallbackQueryHandler(subscriptions_callbacks, pattern="^sub_"))
 
     print("🚀 Бот запущен")
     app.run_polling()
