26920d4128fb113344623af9f026bcecd257a78c
Ark121212124 <Arktiko12@gmail.com>
2026-01-23 21:51:58 +0300

    Delete bot directory

--- bot/config.py
+++
@@ -1,4 +0,0 @@
-import os
-
-TOKEN = os.getenv("TOKEN")
-ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID"))
--- bot/keyboards/subscriptions.py
+++
@@ -1,7 +0,0 @@
-from telegram import InlineKeyboardMarkup, InlineKeyboardButton
-
-SUB_KB = InlineKeyboardMarkup([
-    [InlineKeyboardButton("🔔 Подписаться", callback_data="sub_on")],
-    [InlineKeyboardButton("🔕 Отписаться", callback_data="sub_off")],
-    [InlineKeyboardButton("⬅ Назад", callback_data="menu_main")],
-])
