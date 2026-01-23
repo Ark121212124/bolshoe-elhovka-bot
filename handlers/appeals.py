a989d638da11fe8dfc65dba0a2a14de68ce0414c
Ark121212124 <Arktiko12@gmail.com>
2026-01-23 22:59:10 +0300

    Update appeals.py

--- handlers/appeals.py
+++ handlers/appeals.py
@@ -2,5 +2,21 @@ from telegram import Update
 from telegram.ext import ContextTypes
 
 
-async def appeals_callbacks(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def start_appeal(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.user_data["appeal"] = True
+    await update.message.reply_text(
+        "✉ Напишите ваше обращение одним сообщением."
+    )
+
+
+async def appeals_text_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if context.user_data.get("appeal"):
+        text = update.message.text
+        context.user_data["appeal"] = False
+
+        await update.message.reply_text(
+            "✅ Ваше обращение принято. Спасибо!"
+        )
+        return True
+
     return False
